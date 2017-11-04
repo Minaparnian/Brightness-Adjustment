18e61712bda989ce87ebcbaf937f61ea95d66403
Mina Parnian <minaparnian66@gmail.com>
2017-11-04 12:55:34 +1100

    add input file in html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+function loadImage(){
+  var input, file, fr, img;
+
+  img = input.files[0];
+  fr = new FileReader();
+  fr.onload = createImage;
+  fr.readAsDataURL(file);
+}
+
+
+
+
+
+
+
+
 // function called draw(), which is executed once the page finishes loading
 var image = new Image();
-image.src = "pleasure-garden.jpg";
+image.src = "img/pleasure-garden.jpg";
 function draw() {
   var canvas = document.getElementById('canvas');
   if (canvas.getContext) {
